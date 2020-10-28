bd9a5229f365d60f71186961adfeffb842d37e47
Bakar Chinyoni <chakuchinyoni@gmail.com>
2020-10-28 21:45:24 +0200

    First commit

---
+++ stockmgmgt/static/js/myjs.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  
+  $('.table').paging({limit:4});
+  $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'dd-mm-yy'});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+});
